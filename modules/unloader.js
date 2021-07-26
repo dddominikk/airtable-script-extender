ba1384625c6d2bb1aa70e9d4bdb36b5c7e343732
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-07-27 01:37:26 +0200

    Update and rename (un)loader.js to unloader.js

--- modules/unloader.js
+++ modules/unloader.js
@@ -1,5 +1,12 @@
+/*
+This setup requires a Checkbox field, and optionally, 
+a Button field (that you're looking at). I also tend to
+use it in combination with another Formula field like so:
+IF(ready,"🟢","⚫")
+*/
+
 let rec = { fields: {} },
-    FieldName = `ready`,//change this from 'ready' to the name of your checkbox field
+    FieldName = 'ready', //change this from 'ready' to the name of your checkbox field
     table = base.getTable(cursor.activeTableId),
     record = await input.recordAsync('', table)
         .then(
