10eb89f30c943a3598e937df4f0b1dcfb3a995da
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-08-10 16:17:56 +0200

    Update stateManagementClass.js

--- modules/stateManagementClass.js
+++ modules/stateManagementClass.js
@@ -6,10 +6,18 @@ class module {
     if(typeof namespace === 'string') {
 
       let m = modules.find(module => module
-        .getCellValueAsString('Namespace') === this.namespace)
+        .getCellValueAsString('Namespace') === this.namespace) //this would be the 'label' field from stateManager.js
        
+      
+      // Using eval over a new Function call here for performance reasons, 
+      // BUT you should never go for eval if you're working with raw data.
+      // To illustrate, the database this was originally written in contained
+      // third-normal-form data. And even then the actual evaluation doesn't
+      // touch that data directly; it usually just reads it or maybe processes its clones.
+      
       let Eval = m 
-        ? eval(`(${m.getCellValue('forEval')})`)
+        ? eval(`(${m.getCellValue('forEval')})`) 
+      
         : 'console.error(`No module named ${namespace} found.`)'
 
       module.loaded.push({name:namespace,code:Eval,type:typeof Eval})
@@ -18,3 +26,8 @@ class module {
   }
   static listAll = () => console.log(module.loaded)
 }
+
+
+//usage example, assuming there's a module named 'Random' in the connected database
+
+let Random = new module('Random')
