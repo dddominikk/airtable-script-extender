16901d9dd04eed7ab11f46ca6a69ad7d58892a57
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-08-24 08:45:56 +0200

    Update bowBeforeEval.js

--- modules/bowBeforeEval.js
+++ modules/bowBeforeEval.js
@@ -21,20 +21,25 @@ async function boot(){
     return this.outcome||raceWinner
 }
 
-/* usage: 
+    /*  This script will fully load whatever calling record's activated via the Button field.
+        Here's the script:
+     */
+            await eval(`(${base.getTable('Classes')?.description})`)()
+   
 
+
+    /*  That code up there? That goes into a table description field of your choice.
+
+    usage: 
     1) Copy the above code into any table description (the example here assumes that table is called ‘Classes’)
     2) Create a new script
-    3) Link the script to at least one button field from any table in your base
-        3.1) Optionally, link it to multiple tables
-    4) Drop this one-liner inside the Scripting app/block:
-        
-        ```
-            await eval(`(${base.getTable('Classes')?.description})`)()
-        ```
-
-Naturally, assign that line to a variable ('let someVar = await eval(...')
-if you want to be able to handle the data/continue coding from there.
-
-The code will fully load whatever calling record you activate via the Button field.
+    3a) Link the script to at least one button field from any table in your base
+        3b) Optionally, link it to multiple tables
+        4b) Naturally, assign the above line to a variable ('let someVar = await eval(...') 
+            if you want to be able to handle the data/continue coding from there.
+    5) Have fun
+    6) Never-ever use eval irl
+    7) Let me know if you can think of a way to replicate this functionality with a
+       new Function constructor but without directly referencing the custom input class
+       from the scripting block.
 */
