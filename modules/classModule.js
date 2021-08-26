90d6337d32b0ba5dbbfd505f1a3d6c84ab300442
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2021-08-26 02:58:17 +0200

    Create classModule.js

---
+++ modules/classModule.js
@@ -0,0 +1,21 @@
+class Module {
+
+	static Cache = () => {
+    
+		const 
+			table = base.getTable('Modules'),
+			fields = table.fields.map( f => f.name )
+		
+		return this.Cache = table
+      .selectRecordsAsync( {fields} ) // an empty selectRecordsAsync call is showing up as deprecated as of this week, hence the greedy fields map
+			.then(data => this.Cache = data)
+	}
+
+    constructor(module){
+        base.getTable('Modules')
+		  .createRecordAsync({
+  	  		Name: module.name,
+	  	  	Code: module.toString()
+        })
+    }
+}
