16f9f6653be3cd2fc256db622d33451ae11a8522
Dominik Bošnjak <bosnjakinimod@gmail.com>
2025-06-17 22:19:12 +0200

    added a proper esm from string imported and a b64 guardrail

--- src/modules/importEsmFromString.mjs
+++ src/modules/importEsmFromString.mjs
@@ -1,4 +1,14 @@
-/** @arg {string} srcCode */
-export function importEsmFromString( srcCode ) {
-    return import(`data:application/javascript;base64,${btoa(unescape(encodeURIComponent(sourceCode)))}`);
+import { isBase64 } from '../utils/isBase64.mjs';
+
+/** @type {(s: string) => unknown } */
+export const importEsmFromString = (srcCode) => {
+
+    /** 
+     * if you're importing ESModules from strings, the potentioal for malformed source code is non-zero.
+     * I always use the base64 format as a precaution when working with code that changed platforms.
+     */
+    const b64String = isBase64(srcCode) ? srcCode : btoa(unescape(encodeURIComponent(srcCode)));
+
+    return import(`data:application/javascript;base64,${b64String}`);
+    
 };
