f968e6c3bba8a850dee0748d682ff12fa24d19fb
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2025-08-26 06:50:17 +0200

    Create esmCdnTest.mjs

---
+++ src/utils/esmCdnTest.mjs
@@ -0,0 +1,24 @@
+import {arrayToChunks} from './arrayToChunks.mjs';
+import {wait} from './wait.mjs';
+
+export async function splitArrayWithDelay(arr){
+  const args = [arr].flat(1)
+  if(args.length < 10e3)
+      args.push(...Array.from({length: 10e3 - args.length }, (v,i) => Array(5).fill(Math.random())).flat(1));
+  
+  const halves = [];
+  const midwayPoint = args.length / 2;
+  const toSplit = [args.slice(0, midwayPoint), args.slice(midwayPoint) ];
+
+    for(let i = 0; i < toSplit.length; i++){
+      const obj = arrayToChunks(toSplit[i], toSplit[i].length / 5 ).map(arr=> Object.assign(arr, {chunkSetNo: i}));
+      halves.push(obj);
+      await wait(5000);
+      continue;
+    }
+
+  return {halves, midwayPoint,arr};
+};
+
+export default splitArrayWithDelay;
+export const importedUtils = {arrayToChunks: chunk, wait: sleep };
