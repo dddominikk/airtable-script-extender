5ea0648040189e084cd69fc7f310c70ab4c8db02
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2025-09-13 01:23:01 +0200

    Update dynamicTsImportFromEsmShCdn.mjs

--- tests/dynamicTsImportFromEsmShCdn.mjs
+++ tests/dynamicTsImportFromEsmShCdn.mjs
@@ -1,16 +1,3 @@
-const { writeUrl } = await import(`https://esm.sh/gh/dddominikk/airtable-script-extender/src/utils/writeUrl.ts`);
+const wu = await import(`https://esm.sh/gh/dddominikk/airtable-script-extender/src/utils/writeUrl.ts`);
 
-const writeUrlTests = [
-  { sort: 'score', order: 'asc' },
-  { sort: 'id', order: 'desc', skip: 500 }
-].map(ops => writeUrl('https://api.opencritic.com/api/game', ops));
-
-const testResult = {
-  message: `Successfully imported a Typescript module from a public GitHNub repo.`,
-  data: { 
-    writeUrl,
-    writeUrlTestResults = writeUrlTests
-  }
-};
-
-export default {testResult, meta: import.meta };
+export const meta = import.meta
