9817e199855ceb83e849010322b7113be77ffade
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2025-09-13 00:09:37 +0200

    Create writeUrl.ts

---
+++ src/utils/writeUrl.ts
@@ -0,0 +1,25 @@
+export type UrlParamSchema = Record<string, string | number | boolean | (string | number | boolean)[]>;
+
+export function writeUrl(path: string, params: UrlParamSchema, defaultParams?: UrlParamSchema) {
+
+    const baseParams = Object.assign({}, defaultParams || { sort: 'id', order: 'desc', skip: 0 });
+
+    const mergedParams = Object.assign(baseParams, params || {});
+
+    const filteredParams = Object.entries(mergedParams).filter(([key, value]) => !!value);
+
+    if (filteredParams.length === 0)
+        return path;
+
+    const urlObj = new URL(path);
+
+    filteredParams.forEach(([key, value]) =>
+        urlObj.searchParams.set(key,
+            Array.isArray(value)
+                ? value.join(',')
+                : `${value}`
+        )
+    );
+
+    return urlObj.toString();
+};
