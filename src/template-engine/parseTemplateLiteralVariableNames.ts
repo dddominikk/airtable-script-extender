0e8b3205fec24b5d1f257f354911cfcc0625d284
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-01-19 08:27:42 +0100

    Update parseTemplateLiteralVariableNames.ts

--- src/template-engine/parseTemplateLiteralVariableNames.ts
+++ src/template-engine/parseTemplateLiteralVariableNames.ts
@@ -1,5 +1,12 @@
-export function parseTemplateLiteralVariableNames(templateLiteral: string) {
-    const tlVarPattern = new RegExp(`\\$\{(?<varKey>[^}]+)\}`, 'g');
-    const uniqueVarNames = [...new Set(test?.match(tlVarPattern).map(x => tlVarPattern.exec(x)?.groups?.varKey))].filter(Boolean);
+function parseAllTemplateLiteralVariableNames(templateLiteral: string) {
+    
+    const tlVarPattern = new RegExp(`\\$\{\\s*(?<varKey>[^}]+)\\s*\}`);
+    
+    const allMatches = templateLiteral?.match(new RegExp(tlVarPattern, 'g'));
+    
+    const allVarNames = allMatches?.map(x => tlVarPattern.exec(x)?.groups?.varKey).filter(Boolean);
+    
+    const uniqueVarNames = [...new Set(allVarNames)];
+    
     return uniqueVarNames;
 };
