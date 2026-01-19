9b7efa60efe0d0367ba6a30536ce68b3585eb39c
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-01-19 08:11:56 +0100

    Update index.ts

--- src/template-engine/index.ts
+++ src/template-engine/index.ts
@@ -1,3 +1,2 @@
 export {parseTemplateLiteralVariableNames} from './parseTemplateLiteralVariableNames.ts';
 export {parseTemplateOutline} from './parseTemplateOutline.ts';
-export * as TemplateModule from './parseTemplateOutline.ts';
