03cc23a89e14ec463fbb94563f2683db14843ea2
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-01-19 08:11:11 +0100

    Create index.ts

---
+++ src/template-engine/index.ts
@@ -0,0 +1,3 @@
+export {parseTemplateLiteralVariableNames} from './parseTemplateLiteralVariableNames.ts';
+export {parseTemplateOutline} from './parseTemplateOutline.ts';
+export * as TemplateModule from './parseTemplateOutline.ts';
