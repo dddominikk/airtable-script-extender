5efb05a6c3f73086bae093e86588ede90092a0cb
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-01-28 06:57:11 +0100

    Update index.ts

--- src/utils/time/index.ts
+++ src/utils/time/index.ts
@@ -2,3 +2,4 @@ export {dateDiff} from './dateDiff.ts';
 export {splitByDate} from './splitByDate.ts';
 export {sortEventsByDate} from './sortEventsByDate.ts';
 export {currentTimeContext} from './currentTimeContext.ts';
+export {createTemporalContext} from './createTemporalContext.ts';
