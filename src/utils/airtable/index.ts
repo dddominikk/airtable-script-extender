4a4f7445a72f60200eb7264c7dd7bbac6565e376
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-01-28 02:18:50 +0100

    Export loadAirtableAttachments from index.ts

--- src/utils/airtable/index.ts
+++ src/utils/airtable/index.ts
@@ -1,2 +1,3 @@
 export {loadRecordCellValues} from './loadRecordCellValues.ts';
 export {selectFullRecordsAsync} from './selectFullRecordsAsync.ts';
+export {loadAirtableAttachments} from './loadAirtableAttachments.ts';
