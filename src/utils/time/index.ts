515c9d5f8009ad56a23e4a0cd10220536974722d
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-01-28 02:48:40 +0100

    Export currentTimeContext from index.ts

--- src/utils/time/index.ts
+++ src/utils/time/index.ts
@@ -1,3 +1,4 @@
 export {dateDiff} from './dateDiff.ts';
 export {splitByDate} from './splitByDate.ts';
 export {sortEventsByDate} from './sortEventsByDate.ts';
+export {currentTimeContext} from './currentTimeContext.ts';
