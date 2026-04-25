39f5037710a22bd1285b4277a572fdf8f3a8934a
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-04-25 06:02:47 +0200

    aded loadAndParseAttachments to index.ts exports of modular parser

--- src/modular-parser/index.ts
+++ src/modular-parser/index.ts
@@ -1,3 +1,4 @@
 export { DataParser } from "./DataParser.ts";
 export { parseRaw } from "./parseRawData.ts";
 export { PathResolver } from "./PathResolver.ts";
+export { loadAndParseAttachments } from "./example-airtable-plugin.ts";
