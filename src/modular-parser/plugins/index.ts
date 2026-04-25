af022f387c4b4bfe37020369309e8c93c07fbd5b
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-04-25 07:12:33 +0200

    2026-04-25 07:12:33: semi-automated commit

--- src/modular-parser/plugins/index.ts
+++ src/modular-parser/plugins/index.ts
@@ -1,12 +1,12 @@
-import { DataParser }  from "../DataParser.ts";
+import { DataParser } from "../DataParser.ts";
 import { PathResolver } from "../PathResolver.ts";
-import { parseRaw }     from "../parseRawData.ts";
-import loadPlugins      from "./load.ts";
+import { parseRaw } from "../parseRawData.ts";
+import loadPlugins from "./load.ts";
 
 const plugins = await loadPlugins([
-	{ type: "parser",       dir: "json-parser"  },
-	{ type: "parser",       dir: "mjs-parser"   },
-	{ type: "pathResolver", dir: "url-resolver" },
+	{ type: "parser", dir: "json-parser", name:'jsonParser' },
+	{ type: "parser", dir: "mjs-parser",name:'mjsParser' },
+	{ type: "pathResolver", dir: "url-resolver",name:'urlResolve' },
 ]);
 
 const pluginMap = Object.fromEntries(plugins.map((p) => [p.name, p.instance]));
