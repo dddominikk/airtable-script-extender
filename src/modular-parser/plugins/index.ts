e2ee25655251083bfc760268417e7b6286354fa4
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-04-25 07:33:36 +0200

    2026-04-25 07:33:36: semi-automated commit

--- src/modular-parser/plugins/index.ts
+++ src/modular-parser/plugins/index.ts
@@ -6,7 +6,6 @@ import loadPlugins from "./load.ts";
 const plugins = await loadPlugins([
 	{ type: "parser", dir: "json-parser", name: "jsonParser" },
 	{ type: "parser", dir: "mjs-parser", name: "mjsParser" },
-	{ type: "pathResolver", dir: "url-resolver", name: "urlResolve" },
 	{ type: "custom", dir: "url-resolver", name: "urlResolve2" },
 ]);
 
