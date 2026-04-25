da8309a23d56da66e5c145b5b9268eabff14189a
Dominik Bošnjak <bosnjakinimod@gmail.com>
2026-04-25 06:14:52 +0200

    2026-04-25 06:14:51: semi-automated commit

--- src/modular-parser/index.ts
+++ src/modular-parser/index.ts
@@ -1,4 +1,6 @@
 export { DataParser } from "./DataParser.ts";
 export { parseRaw } from "./parseRawData.ts";
 export { PathResolver } from "./PathResolver.ts";
+export * as Plugins from './plugins/index.ts';
 export { loadAndParseAttachments } from "./example-airtable-plugin.ts";
+
